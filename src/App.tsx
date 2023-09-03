bcc7de991f2107b4a7b1e07664567c8d23970ee7
aramfilak <aram.filak@outlook.de>
2023-09-03 20:31:43 +0200

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MenuWrapper from './components/MenuWrapper';
 import GamePage from './pages/game_page/GamePage';
 import StartMenu from './components/StartMenu';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <main className="memory-game">
+      <Analytics />
       <GamePage />
       <MenuWrapper>
         <StartMenu />
